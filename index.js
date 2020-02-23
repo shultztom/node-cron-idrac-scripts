82401b260f412ca3df0ff7b14cf89d380ab4d41c
shultztom <shultzthomask@sau.edu>
2020-02-22 19:15:27 -0600

    fixed orderof functions

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const shell = require("shelljs");
 const cron = require("node-cron");
 
 shell.env["idracPass"] = process.env.IDRAC_PASS;
+const setFans = () => {
+  shell.exec(__dirname + "/r310-set-fans.sh");
+  shell.exec(__dirname + "/r610-set-fans.sh");
+  shell.exec(__dirname + "/r710-set-fans.sh");
+};
+
+const healthCheck = () => {
+  shell.exec(
+    `curl -fsS --retry 3 https://hc-ping.com/${process.env.HEALTH_CHECK_KEY} > /dev/null`
+  );
+};
 
 // First Run
 setFans();
@@ -15,15 +26,3 @@ cron.schedule("*/2 * * * *", () => {
 cron.schedule("0 */8 * * *", () => {
   healthCheck();
 });
-
-const setFans = () => {
-  shell.exec(__dirname + "/r310-set-fans.sh");
-  shell.exec(__dirname + "/r610-set-fans.sh");
-  shell.exec(__dirname + "/r710-set-fans.sh");
-};
-
-const healthCheck = () => {
-  shell.exec(
-    `curl -fsS --retry 3 https://hc-ping.com/${process.env.HEALTH_CHECK_KEY} > /dev/null`
-  );
-};
