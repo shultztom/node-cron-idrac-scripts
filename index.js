b372962f2fdb1ba871f15554ae7e04c4bde7c157
shultztom <shultzthomask@sau.edu>
2020-02-22 19:10:57 -0600

    added heath check ping

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const cron = require("node-cron");
 shell.env["idracPass"] = process.env.IDRAC_PASS;
 
 // First Run
-shell.exec(__dirname + "/r610-set-fans.sh");
-shell.exec(__dirname + "/r710-set-fans.sh");
-shell.exec(__dirname + "/r310-set-fans.sh");
+setFans();
+healthCheck();
 
 cron.schedule("*/2 * * * *", () => {
+  setFans();
+});
+
+cron.schedule("0 */8 * * *", () => {
+  healthCheck();
+});
+
+const setFans = () => {
+  shell.exec(__dirname + "/r310-set-fans.sh");
   shell.exec(__dirname + "/r610-set-fans.sh");
   shell.exec(__dirname + "/r710-set-fans.sh");
-  shell.exec(__dirname + "/r310-set-fans.sh");
-});
+};
+
+const healthCheck = () => {
+  shell.exec(
+    `curl -fsS --retry 3 https://hc-ping.com/${process.env.HEALTH_CHECK_KEY} > /dev/null`
+  );
+};
